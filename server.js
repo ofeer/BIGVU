a620a69ac5f1347aa068f7c513233970afe91844
ofeer <mintz.ofeer@gmail.com>
2021-06-08 11:11:38 +0300

    final

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ import ffmpegPath  from '@ffmpeg-installer/ffmpeg';
 import ffmpeg  from 'fluent-ffmpeg';
 
 
-
 ffmpeg.setFfmpegPath(ffmpegPath.path);
 
 const port = 3000;
@@ -40,9 +39,9 @@ app.post('/', async (req, res) =>  {
     // get Json input
     url = req.body.url;
 
-
+    // get current dir
     const dirname = (process.cwd() + "\\video.mp4");
-    console.log(dirname);
+
     // delete files if exists
     deleteFile('screenshot.png')
     deleteFile('video.mp4')
